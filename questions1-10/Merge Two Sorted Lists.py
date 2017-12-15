cd7c35e9b007dc3e0a665e26f05e6787fa127633
boweiww <boweiww@gmail.com>
2017-12-14 21:28:41 -0700

    2017/12/14

--- questions1-10/Merge Two Sorted Lists.py
+++ questions1-10/Merge Two Sorted Lists.py
@@ -42,3 +42,68 @@ class Solution(object):
                             return list
                         l2=l2.next
                 l1=l1.next
+                
+                
+                
+# solution of leetcode:
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+        
+# solution from other leetcode users:
+
+    # iteratively
+    # Assume ListNode is okay to call (that is not built in on my computer)
+    # This is an interesting algorithm
+    # keep resetting current node
+    def mergeTwoLists1(self, l1, l2):
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
+        
+    # recursively    this way is similar to official way
+    def mergeTwoLists2(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+            
+    # in-place, iteratively        
+    def mergeTwoLists(self, l1, l2):
+        if None in (l1, l2):
+            return l1 or l2
+        dummy = cur = ListNode(0)
+        dummy.next = l1
+        while l1 and l2:
+            if l1.val < l2.val:
+                l1 = l1.next
+            else:
+                nxt = cur.next
+                cur.next = l2
+                tmp = l2.next
+                l2.next = nxt
+                l2 = tmp
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
